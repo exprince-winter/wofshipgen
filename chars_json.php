ae15929a90612d4e0505e8bed9368d3d388e5222
Winter <165444013+exprince-winter@users.noreply.github.com>
2024-04-23 07:54:55 -0700

    Delete chars_json.php
    probably broken

--- chars_json.php
+++
@@ -1,9 +0,0 @@
-<?php 
-header('Content-Type: application/json');
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: *");
-header("Access-Control-Allow-Methods: GET");
-include('inc/array.php'); 
-echo '{"wofchars":' . json_encode($array) . '}';
-
-?>
